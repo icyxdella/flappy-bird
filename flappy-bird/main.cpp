1c39c4e20e1301d4001ffa036953e5ad3a3d8abe
Georgiana Cociorba <icyxdella@gmail.com>
2017-07-29 00:37:52 +0300

    learning

--- flappy-bird/main.cpp
+++ flappy-bird/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+using namespace std;
+
+int factorial(int x) {
+	int  rezultat = 1;
+	for (int i = 1; i <= x; i++)
+	{
+		rezultat = rezultat * i;
+	}
+	return rezultat;
+}
 int main()
 {
-	int i;
+
+	int x = 0, q = 0, w = 0, e = 0, r = 0;
+
+	cout << "Introduceti x";
+	cin >> x >> q >> w >> e >> r ;
+	
+	cout << factorial(x) << " " << factorial(q) << " " << factorial(w) << " " << factorial(e) << " " << factorial(r);
+	system("PAUSE");
 	return 0;
 }
